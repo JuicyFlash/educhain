e8b37120ffc640bd0ca81ebec5e170eb5d9dc328
JuicyFlash <tankedet@bk.ru>
2025-01-11 23:30:04 +0300

    Слоты

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   def index
     @courses = CourseComponent.with_collection(Course.all, notice: "Buy Me")
+    @course_titles = Course.all.pluck(:title).map { |title| { title: title } }
   end
 end
