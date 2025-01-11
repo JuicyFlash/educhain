00a63cc887a6e22ad9d6b534d14fda76bed557b2
JuicyFlash <tankedet@bk.ru>
2025-01-11 22:41:35 +0300

    preview

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Educhain
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.view_component.preview_paths << "#{Rails.root}/lib/component_previews"
   end
 end
